001538a9555609709694b712dce42428c37e648c
maou-shonen <q267009886.tw@gmail.com>
2024-06-28 15:36:20 +0800

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { logger } from "./logger";
 export type { Options } from "./logger";
 export { getLogger } from "./utils";
+export * from "./types";
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,7 @@
-import type pino from "pino";
 import type { PinoLogger } from "./logger";
 
 declare module "hono" {
   export interface ContextVariableMap {
     logger: PinoLogger;
   }
-}
-
-export interface LoggerContext {
-  Variables: {
-    logger: pino.Logger;
-  };
 }
