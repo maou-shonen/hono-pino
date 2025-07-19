071e8061e88d1b7c5961ea52056c6fe61bc175df
maou shonen <q267009886.tw@gmail.com>
2025-07-19 20:51:53 +0000

    Remove examples  test and update Vitest configuration to exclude example tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "edge-runtime", // runtime agnostic
-    include: ["src/**/*.test.ts", "examples/**/*.test.ts"],
+    include: ["src/**/*.test.ts"],
     coverage: {
       enabled: true,
       include: ["src/**/*.ts"],
