ad725fd7fd7a866f27e044edba1429918bd4ef5d
魔王少年(maou shonen) <q267009886.tw@gmail.com>
2026-04-04 22:14:59 +0800

    fix: resolve open dependabot security alerts (#140)

--- examples/cloudflare-workers_vite/src/index.ts
+++ examples/cloudflare-workers_vite/src/index.ts
@@ -1,9 +1,13 @@
+import type { MiddlewareHandler } from "hono";
 import { Hono } from "hono";
+import type { Env as HonoPinoEnv } from "../../../src";
 import { pinoLogger } from "../../../src"; // hono-pino package
 
-const app = new Hono();
+const app = new Hono<HonoPinoEnv>();
 
-app.use(pinoLogger());
+const loggerMiddleware: MiddlewareHandler<HonoPinoEnv, "*"> = pinoLogger();
+
+app.use("*", loggerMiddleware);
 
 app.get("/", (c) => {
   return c.text("hello world");
